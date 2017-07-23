28a6117e83a3ab823c63be610f160fdc57a81fa1
dequiroga <de.quiroga@uniandes.edu.co>
2017-07-22 20:47:15 -0700

    Se intento leer el archivo de datos en C pero da seg fault. Se revisaron y estudiaron los datos en Plots.py

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -1,1 +1,85 @@
 //Encuentra el punto nemo. 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+//Define funciones
+//float menor_radio();
+int I(int lat, int lng);
+
+//Funciones
+int I(int lat, int lng){
+	int i;
+	i=lat*744+lng;
+	return i;
+}
+
+//Main
+
+int main(void){
+
+	//Lee archivo
+	FILE *in;
+	in=fopen("map_data.txt","r");
+	//Contadores para leer
+	int i=0;
+	int j=0;
+
+	//Delimiter
+	int len=10;
+	char datos[len];
+	char *dato_I;
+	const char *delimiter;
+	delimiter = " ,-.";
+
+	//Matriz donde se guardan los datos leidos
+	int *mapa;
+	mapa=malloc(744*500*sizeof(int));
+
+	
+	while(fgets(datos, len ,in)){
+		dato_I=strtok(datos, delimiter);
+			while(dato_I != NULL){
+				mapa[I(i,j)]=atoi(dato_I);
+				j++;
+				dato_I=strtok(NULL, delimiter);
+				printf("%d\n", j);
+			}
+		//printf("%d\n", i);
+		i++;
+		j=0;
+	}
+
+	fclose(in);	
+
+	//Prueba de lectura de datos
+	int c;
+	int d;
+	for(c=0;c<500;c++){
+		for(d=0;d<744;d++){
+			printf("dato (%d,%d): %d\n",c,d, mapa[I(c,d)]);
+		}
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Plots.py
+++ Plots.py
@@ -1,1 +1,10 @@
 #Genera las graficas de los datos que se obtienen de GraphicPoint.c
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+mapa=np.loadtxt('map_data.txt')
+#Arreglo de 500(Latitud)x744(longitud) con el mapa de la tierra, 0 si es mar y 1 si es tierra
+
+plt.imshow(mapa, cmap='Greens')
+plt.show()
