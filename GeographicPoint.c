f4b9db7b60f4cbbb443c8b612d6860e7702576ca
dequiroga <de.quiroga@uniandes.edu.co>
2017-07-25 20:33:21 -0700

    Se hizo el primer itento de encontrar las cordenadas del punto nemo con metodo MH. las coordenadas se salen del mapa.

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -11,8 +11,9 @@
 //Define funciones
 int I(int lat, int lng);
 float radio(int lat_p, int lng_p, int lat, int lng);
-float menor_radio(int *mapa);
-
+float menor_radio(int *mapa, int lat_p, int lng_p);
+int rdm(int centro);
+int *MH(int *mapa);
 
 //Funciones
 int I(int lat, int lng){
@@ -31,24 +32,75 @@ float radio(int lat_p, int lng_p, int lat, int lng){
 	return R;
 }
 
-float menor_radio(int *mapa){
+float menor_radio(int *mapa, int lat_p, int lng_p){
 	int i;
 	int j;
-	int lat_p=rand() % max_lat;
-	int lng_p=rand() % max_lng;
+	//int lat_p=rand() % max_lat;
+	//int lng_p=rand() % max_lng;
 	float R1=1000.0;
-	for(i=0;i<max_lat;i++){
-		for(j=0;j<max_lng;j++){
-			if(mapa[I(i,j)]==1){
-				float R2;
-				R2=radio(lat_p, lng_p, i, j);
-					if(R2<R1){
-						R1=R2;
-					}
+
+	if(lat_p>=500 || lng_p>=744){
+		printf("%s\n", "Coordenada fuera de rango");
+	}
+	else{
+		for(i=0;i<max_lat;i++){
+			for(j=0;j<max_lng;j++){
+				if(mapa[I(i,j)]==1){
+					float R2;
+					R2=radio(lat_p, lng_p, i, j);
+						if(R2<R1){
+							R1=R2;
+						}
+				}
 			}
 		}
 	}
 	return R1;
+}
+
+int rdm(int centro){
+	int x;
+	int max=centro+100;
+	int min=centro-100;
+	x=(((max-min)*((int) rand()/RAND_MAX))+min);
+	return x;
+}
+
+int *MH(int *mapa){
+	
+	int *Nemo;
+	Nemo=malloc(2*sizeof(int));
+
+	int n_iterations=20000;
+	int lat_0=390;
+	int lng_0=100;	
+
+	//Loop de Metropolis-Hasting
+	int i;
+	for(i=0;i<n_iterations;i++){
+		int lat_prime = rdm(lat_0);
+		int lng_prime = rdm(lng_0);
+		float alpha=menor_radio(mapa, lat_prime, lng_prime)/menor_radio(mapa, lat_0, lng_0);
+		if(alpha>=1.0){
+			lat_0=lat_prime;
+			lng_0=lng_prime;
+		}
+		 else{
+			float beta =(float) rand()/RAND_MAX;
+			if(beta<=alpha){
+				lat_0=lat_prime;
+				lng_0=lng_prime;
+			}
+			else{
+				continue;
+			}
+		}
+	}
+	
+	Nemo[0]=lat_0;
+	Nemo[1]=lng_0;
+
+	return Nemo;
 }
 
 //Main
@@ -79,9 +131,7 @@ int main(void){
 				mapa[I(i,j)]=atoi(dato_I);
 				j++;
 				dato_I=strtok(NULL, delimiter);
-				printf("%d\n", j);
 			}
-		//printf("%d\n", i);
 		i++;
 		j=0;
 	}
@@ -89,13 +139,16 @@ int main(void){
 	fclose(in);	
 
 	//Prueba de lectura de datos
-	int c;
-	int d;
-	for(c=0;c<max_lat;c++){
-		for(d=0;d<max_lng;d++){
-			printf("dato (%d,%d): %d\n",c,d, mapa[I(c,d)]);
-		}
-	}
+	//int c;
+	//int d;
+	//for(c=0;c<max_lat;c++){
+	//	for(d=0;d<max_lng;d++){
+	//		printf("dato (%d,%d): %d\n",c,d, mapa[I(c,d)]);
+	//	}
+	//}
+
+	printf("%d\n", MH(mapa)[0]);
+	printf("%d\n", MH(mapa)[1]);
 
 	return 0;
 }
--- Plots.py
+++ Plots.py
@@ -7,4 +7,11 @@ mapa=np.loadtxt('map_data.txt')
 #Arreglo de 500(Latitud)x744(longitud) con el mapa de la tierra, 0 si es mar y 1 si es tierra
 
 plt.imshow(mapa, cmap='Greens')
+#plt.show()
+plt.close()
+
+x=np.linspace(-5,5,1000)
+f=np.exp(-((x)**2)/0.1);
+
+plt.plot(x,f)
 plt.show()
