806f2c4505c61a7bcc9fa5903b638aa1e81e6bb0
dequiroga <de.quiroga@uniandes.edu.co>
2017-07-26 09:32:28 -0700

    Se leyo el punto nemo que sale del programa en c y se grafico en el mapa. No se sabe si este bien

--- Plots.py
+++ Plots.py
@@ -6,12 +6,11 @@ import matplotlib.pyplot as plt
 mapa=np.loadtxt('map_data.txt')
 #Arreglo de 500(Latitud)x744(longitud) con el mapa de la tierra, 0 si es mar y 1 si es tierra
 
+PN=np.loadtxt('coordNemo.csv', delimiter=',')
+
+plt.scatter(PN[1],PN[0], c='red')
+
 plt.imshow(mapa, cmap='Greens')
-#plt.show()
+plt.show()
 plt.close()
 
-x=np.linspace(-5,5,1000)
-f=np.exp(-((x)**2)/0.1);
-
-plt.plot(x,f)
-plt.show()
