75a396d6e0862612ab6953041a283270ec3f5bb7
dequiroga <de.quiroga@uniandes.edu.co>
2017-07-26 09:31:53 -0700

    Se soluciono error de salirse del tablero con modulo, (frontera periodica) y se arreglo problema con sigos de cordenadas. Se calculo punto nemo

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -4,6 +4,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
+#include<time.h>
 
 #define max_lat 500
 #define max_lng 744
@@ -39,10 +40,10 @@ float menor_radio(int *mapa, int lat_p, int lng_p){
 	//int lng_p=rand() % max_lng;
 	float R1=1000.0;
 
-	if(lat_p>=500 || lng_p>=744){
-		printf("%s\n", "Coordenada fuera de rango");
-	}
-	else{
+	//if(lat_p>=500 || lng_p>=744){
+	//	printf("%s\n", "Coordenada fuera de rango");
+	//}
+	//else{
 		for(i=0;i<max_lat;i++){
 			for(j=0;j<max_lng;j++){
 				if(mapa[I(i,j)]==1){
@@ -54,15 +55,15 @@ float menor_radio(int *mapa, int lat_p, int lng_p){
 				}
 			}
 		}
-	}
+	//}
 	return R1;
 }
 
 int rdm(int centro){
 	int x;
-	int max=centro+100;
-	int min=centro-100;
-	x=(((max-min)*((int) rand()/RAND_MAX))+min);
+	int max=centro+300;
+	int min=centro-300;
+	x=(((max-min)*((float) rand()/RAND_MAX))+min);
 	return x;
 }
 
@@ -71,34 +72,66 @@ int *MH(int *mapa){
 	int *Nemo;
 	Nemo=malloc(2*sizeof(int));
 
-	int n_iterations=20000;
+	int n_iterations=2000;
 	int lat_0=390;
 	int lng_0=100;	
 
+	int *LAT;
+	LAT=malloc(n_iterations*sizeof(int));
+
+	int *LNG;
+	LNG=malloc(n_iterations*sizeof(int));
+
+	float *R;
+	R=malloc(n_iterations*sizeof(float));
+
+	LAT[0]=lat_0;
+	LNG[0]=lng_0;
+
 	//Loop de Metropolis-Hasting
 	int i;
-	for(i=0;i<n_iterations;i++){
+	for(i=1;i<n_iterations;i++){
 		int lat_prime = rdm(lat_0);
+		lat_prime=lat_prime%max_lat;
 		int lng_prime = rdm(lng_0);
+		lng_prime=lng_prime%max_lng;
+		if(lat_prime<0){
+			lat_prime=-lat_prime;
+		}
+		if(lng_prime<0){
+			lng_prime=-lng_prime;
+		}
+		//printf("%d\n", lat_prime);
+		//printf("%d\n", lng_prime);
 		float alpha=menor_radio(mapa, lat_prime, lng_prime)/menor_radio(mapa, lat_0, lng_0);
 		if(alpha>=1.0){
-			lat_0=lat_prime;
-			lng_0=lng_prime;
+			LAT[i]=lat_prime;
+			LNG[i]=lng_prime;
 		}
-		 else{
+		else{
 			float beta =(float) rand()/RAND_MAX;
 			if(beta<=alpha){
-				lat_0=lat_prime;
-				lng_0=lng_prime;
+				LAT[i]=lat_prime;
+				LNG[i]=lng_prime;
 			}
 			else{
-				continue;
+				LAT[i]=LAT[i-1];
+				LNG[i]=LNG[i-1];
 			}
 		}
 	}
 	
-	Nemo[0]=lat_0;
-	Nemo[1]=lng_0;
+	//Promedio 
+	int LAT_NEMO=0;
+	int LNG_NEMO=0;
+	int c;
+	for(c=0;c<n_iterations;c++){
+		LAT_NEMO=LAT_NEMO+LAT[c];
+		LNG_NEMO=LNG_NEMO+LNG[c];
+	}
+	
+	Nemo[0]=LAT_NEMO/n_iterations;
+	Nemo[1]=LNG_NEMO/n_iterations;
 
 	return Nemo;
 }
@@ -112,6 +145,8 @@ int main(void){
 	//Contadores para leer
 	int i=0;
 	int j=0;
+	//seed
+	srand((unsigned)time(NULL));
 
 	//Delimiter
 	int len=2000;
@@ -149,6 +184,22 @@ int main(void){
 
 	printf("%d\n", MH(mapa)[0]);
 	printf("%d\n", MH(mapa)[1]);
+
+	//Se escribe un archivo con los datos
+	FILE *out;
+	int c; //Contador para tiempo
+	int n; //Contador para planeta
+	char resultados[200]="coordNemo.csv";
+
+	out=fopen(resultados, "w");
+	if(!out){
+		printf("problems opening the file %s\n", resultados);
+		exit(1);
+	} 
+
+	//Se escriben las coordenadas del punto Nemo(indices en el mapa).
+		fprintf(out, "%d, %d\n", MH(mapa)[0], MH(mapa)[1]);
+
 
 	return 0;
 }
